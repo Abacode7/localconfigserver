84b90b8c3ee62760f96903cc336be10c717664f3
Abraham Olamipo <aolamipo@teamapt.com>
2022-11-14 07:51:45 +0100

    Initial commit

---
+++ src/test/java/com/pluralsight/localconfigserver/LocalconfigserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pluralsight.localconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocalconfigserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
